79ecd1c016c2630a4ad73342ee5c58c5f6070a08
LinjianLi <linjiann.li@gmail.com>
2019-04-28 09:58:47 +0800

    Adjust Number of Threads

--- run_all_in_bin.sh
+++ run_all_in_bin.sh
@@ -2,5 +2,5 @@ cd bin
 
 ./MtrxMul_Serial 4096 0.3
 ./MtrxMul_OMP 4096 0.3
-./MtrxMul_Blocked_OMP 4096 0.3 12
-./MtrxMul_SIMD_OMP 4096 0.3 12
+./MtrxMul_Blocked_OMP 4096 0.3 24
+./MtrxMul_SIMD_OMP 4096 0.3 24
